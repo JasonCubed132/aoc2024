ff6eba758ee77be44dbee42922126cabbd54b8bd
JasonCubed132 <amcb2002@googlemail.com>
2024-12-03 18:55:51 +0000

    Rewrite count_items_in_list using iterators

--- src/days/list_ops.rs
+++ src/days/list_ops.rs
@@ -1,21 +1,8 @@
 use std::collections::HashMap;
 
 pub fn count_items_in_list<T: Eq + std::hash::Hash>(input: &Vec<T>) -> HashMap<&T, i32> {
-    let mut counts = HashMap::new();
-
-    for item in input {
-        let existing_value = counts.get(&item);
-        let new_value;
-        match existing_value {
-            Some(x) => {
-                new_value = x + 1;
-            }
-            None => {
-                new_value = 1;
-            }
-        }
-        counts.insert(item, new_value);
-    }
-
-    counts
+    input.into_iter().fold(HashMap::new(), |mut acc, item| {
+        acc.entry(&item).and_modify(|val| *val += 1).or_insert(1);
+        acc
+    })
 }
